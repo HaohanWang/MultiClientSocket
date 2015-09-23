aa3ef4ae752961ed5f48a97d5a7c5d29333e4d25
Haohan Wang <haohanw@andrew.cmu.edu>
2015-09-23 15:34:47 -0400

    original files for cpp server/client

---
+++ CPPClient/client.h
@@ -0,0 +1,8 @@
+//
+// Created by haohanwang on 9/23/15.
+//
+
+#ifndef CPPCLIENT_CLIENT_H
+#define CPPCLIENT_CLIENT_H
+
+#endif //CPPCLIENT_CLIENT_H
---
+++ SocketServer/server.h
@@ -0,0 +1,8 @@
+//
+// Created by haohanwang on 9/23/15.
+//
+
+#ifndef SOCKETSERVER_SERVER_H
+#define SOCKETSERVER_SERVER_H
+
+#endif //SOCKETSERVER_SERVER_H
