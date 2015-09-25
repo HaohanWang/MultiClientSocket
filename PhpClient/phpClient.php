3d2fcd22665ea7a8741cb8c5bf1ebb5ac7134d42
Haohan Wang <haohanw@andrew.cmu.edu>
2015-09-25 18:02:45 -0400

    the first runnable php...

---
+++ PhpClient/phpClient.php
@@ -0,0 +1,19 @@
+
+/**
+ * Created by PhpStorm.
+ * User: haohanwang
+ * Date: 9/25/15
+ * Time: 10:49 AM
+ */
+
+<html>
+
+   <head>
+      <title>Hello World</title>
+   </head>
+
+   <body>
+      <?php echo "Hello, World!";?>
+</body>
+
+</html>
