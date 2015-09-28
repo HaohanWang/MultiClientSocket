d9ede99baa02812e7be8585a50aff80be58f345f
Haohan Wang <haohanw@andrew.cmu.edu>
2015-09-28 14:54:36 -0400

    php client socket formatted

--- PhpClient/phpClient.php
+++ PhpClient/phpClient.php
@@ -13,45 +13,58 @@
 
 <body>
 <?php
-if (!($sock = socket_create(AF_INET, SOCK_STREAM, 0))) {
-    $errorcode = socket_last_error();
-    $errormsg = socket_strerror($errorcode);
+global $sock;
+function connect(){
+    global $sock;
+    if (!($sock = socket_create(AF_INET, SOCK_STREAM, 0))) {
+        $errorcode = socket_last_error();
+        $errormsg = socket_strerror($errorcode);
 
-    die("Couldn't create socket: [$errorcode] $errormsg \n");
+        die("Couldn't create socket: [$errorcode] $errormsg \n");
+    }
+
+    echo "Socket created";
+
+    if (!socket_connect($sock, 'localhost', 55777)) {
+        $errorcode = socket_last_error();
+        $errormsg = socket_strerror($errorcode);
+
+        die("Could not connect: [$errorcode] $errormsg \n");
+    }
+
+    echo "Connection established <br>";
 }
 
-echo "Socket created";
+function send_message($message){
+    global $sock;
+    if (!socket_send($sock, $message, strlen($message), 0)) {
+        $errorcode = socket_last_error();
+        $errormsg = socket_strerror($errorcode);
 
-if (!socket_connect($sock, 'localhost', 55777)) {
-    $errorcode = socket_last_error();
-    $errormsg = socket_strerror($errorcode);
+        die("Could not send data: [$errorcode] $errormsg \n");
+    }
 
-    die("Could not connect: [$errorcode] $errormsg \n");
+    echo "Message send successfully<br>";
 }
-
-echo "Connection established <br>";
 
 $message = "{\"size\":3,\"type\":\"float\",\"class\":\"vector\",\"value\":{\"0\":0.89291495,\"1\":0.6799756,\"2\":0.10601133}}";
 
 //Send the message to the server
-if (!socket_send($sock, $message, strlen($message), 0)) {
-    $errorcode = socket_last_error();
-    $errormsg = socket_strerror($errorcode);
 
-    die("Could not send data: [$errorcode] $errormsg \n");
+function get_message(){
+    global $sock;
+    if (socket_recv($sock, $buf, 65535, MSG_WAITALL) === FALSE) {
+        $errorcode = socket_last_error();
+        $errormsg = socket_strerror($errorcode);
+
+        die("Could not receive data: [$errorcode] $errormsg \n");
+    }
+    return $buf;
 }
 
-echo "Message send successfully<br>";
-
-if (socket_recv($sock, $buf, 65535, MSG_WAITALL) === FALSE) {
-    $errorcode = socket_last_error();
-    $errormsg = socket_strerror($errorcode);
-
-    die("Could not receive data: [$errorcode] $errormsg \n");
-}
-
-//print the received message
-echo $buf;
+connect();
+send_message($message);
+echo get_message();
 
 ?>
 </body>
