62be5562dc748e38d564bcc3b1cee64ed63fcde6
Haohan Wang <haohanw@andrew.cmu.edu>
2015-09-28 16:51:21 -0400

    json encoder/decoder

--- PhpClient/phpClient.php
+++ PhpClient/phpClient.php
@@ -13,8 +13,71 @@
 
 <body>
 <?php
-global $sock;
-function connect(){
+
+function startsWith($haystack, $needle, $case = true)
+{
+    if ($case)
+        return strpos($haystack, $needle, 0) === 0;
+
+    return stripos($haystack, $needle, 0) === 0;
+}
+
+function endsWith($haystack, $needle, $case = true)
+{
+    $expectedPosition = strlen($haystack) - strlen($needle);
+
+    if ($case)
+        return strrpos($haystack, $needle, 0) === $expectedPosition;
+
+    return strripos($haystack, $needle, 0) === $expectedPosition;
+}
+
+function json_encoder_format($var)
+{
+    if (gettype($var) == "array") {
+        $class = '"vector"';
+        $type = gettype($var[0]);
+        if ($type == "double"){
+            $type = "float";
+        }
+        $type = '"'.$type .'"';
+        $size = count($var);
+        $str = '{"class":' . $class . ',"type":' . $type . ',"size":' . $size . ',"value":{';
+        for ($a = 0; $a < $size - 1; $a++) {
+            $str = $str . '"' . $a . '":' . $var[$a] . ',';
+        }
+        $ind = $size - 1;
+        $str = $str . '"' . $ind . '":' . $var[$size - 1] . '}}';
+
+        return $str;
+    } else {
+        $class = "single_value";
+        $type = gettype($var);
+        $size = 1;
+        $str = '{"class":' . $class . ',"type":' . $type . ',"size":' . $size . ',"value":' . $var . '}';
+        return $str;
+    }
+}
+
+function json_decoder_single_value($message){
+    $var = json_decode($message);
+    $value = $var->{"value"};
+    return $value;
+}
+
+function json_decoder_vector($message){
+    $var = json_decode($message);
+    $size = $var->{"size"};
+    $value = [];
+    for ($a=0;$a<$size;$a++){
+        $value[] = $var->{"value"}->{$a};
+    }
+    return $value;
+}
+
+
+function connect()
+{
     global $sock;
     if (!($sock = socket_create(AF_INET, SOCK_STREAM, 0))) {
         $errorcode = socket_last_error();
@@ -35,7 +98,8 @@ function connect(){
     echo "Connection established <br>";
 }
 
-function send_message($message){
+function send_message($message)
+{
     global $sock;
     if (!socket_send($sock, $message, strlen($message), 0)) {
         $errorcode = socket_last_error();
@@ -47,11 +111,8 @@ function send_message($message){
     echo "Message send successfully<br>";
 }
 
-$message = "{\"size\":3,\"type\":\"float\",\"class\":\"vector\",\"value\":{\"0\":0.89291495,\"1\":0.6799756,\"2\":0.10601133}}";
-
-//Send the message to the server
-
-function get_message(){
+function get_message()
+{
     global $sock;
     if (socket_recv($sock, $buf, 65535, MSG_WAITALL) === FALSE) {
         $errorcode = socket_last_error();
@@ -59,12 +120,20 @@ function get_message(){
 
         die("Could not receive data: [$errorcode] $errormsg \n");
     }
-    return $buf;
+    $pos = strpos($buf, "\0");
+    return substr($buf, 0, $pos);
 }
+
+//$message = "{\"size\":3,\"type\":\"float\",\"class\":\"vector\",\"value\":{\"0\":0.89291495,\"1\":0.6799756,\"2\":0.10601133}}";
+//
+$variable = array(0.123, 0.234, 0.566, 0.001, 0.345);
+$message = json_encoder_format($variable);
 
 connect();
 send_message($message);
-echo get_message();
+$message = get_message();
+$var = json_decoder_vector($message);
+echo var_dump($var);
 
 ?>
 </body>
